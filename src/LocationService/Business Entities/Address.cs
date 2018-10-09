9c8aea506c257bb7d29bb6600345425123eef2e7
Kate Orlova <kate.orlova@deleteagency.com>
2018-10-10 00:19:07 +0100

    Added the initial project with reference to PostcodeEverywhere

---
+++ src/LocationService/Business Entities/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace LocationService.Business_Entities
+{
+    public class Address
+    {
+        public string AddressLine1;
+        public string AddressLine2;
+        public string Town;
+        public string County;
+        public string PostCode;
+        public string HouseNo;
+        public string ErrorMessage;
+        public string[] HouseNumbers;
+        public double? Latitude;
+        public double? Longitude;
+    }
+}
