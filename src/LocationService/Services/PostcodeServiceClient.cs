70acac0e2962f39744d914cdb3de612f9c749c9e
Kate Orlova <kate.orlova@deleteagency.com>
2018-10-12 22:54:40 +0100

    Added the postcode service client

---
+++ src/LocationService/Services/PostcodeServiceClient.cs
@@ -0,0 +1,23 @@
+﻿using LocationService.Business_Entities;
+using LocationService.Interfaces;
+
+namespace LocationService.Services
+{
+    public class PostcodeServiceClient
+    {
+        private readonly IAddressInfoProvider _serviceClient = null;
+        public bool Enabled { get; } = true;
+
+        public PostcodeServiceClient(IAddressInfoProviderFactory addressInfoProviderFactory)
+        {
+            _serviceClient = addressInfoProviderFactory.CreateProvider();
+            if (_serviceClient == null)
+                Enabled = false;
+        }
+
+        public Address FindAddress(string postCode)
+        {
+            return _serviceClient.AddressLookup(postCode);
+        }
+    }
+}
