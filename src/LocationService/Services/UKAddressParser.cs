c27537f43fd5596a150262f8f8fb22c316285ebd
Kate Orlova <kate.orlova@deleteagency.com>
2018-10-16 23:32:52 +0100

    implemented the address parsing method

--- src/LocationService/Services/UKAddressParser.cs
+++ src/LocationService/Services/UKAddressParser.cs
@@ -8,7 +8,37 @@ namespace LocationService.Services
     {
         public Address Parse(string addressString, Address baseAddress)
         {
-            throw new NotImplementedException();
+            var addressResult = new Address()
+            {
+                HouseNo = addressString,
+                AddressLine1 = baseAddress.AddressLine1,
+                AddressLine2 = baseAddress.AddressLine2,
+                County = baseAddress.County,
+                ErrorMessage = "",
+                PostCode = baseAddress.PostCode,
+                Town = baseAddress.Town
+            };
+
+            // remove postcode
+            addressResult.HouseNo = addressResult.HouseNo.Substring(addressResult.PostCode.Length);
+
+            // remove county
+            addressResult.HouseNo = addressResult.HouseNo.IndexOf(baseAddress.County, StringComparison.InvariantCultureIgnoreCase) == addressResult.HouseNo.Length - baseAddress.County.Length ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.County.Length) : addressResult.HouseNo;
+            addressResult.HouseNo = addressResult.HouseNo.Trim(' ').Trim(',');
+
+            // remove town
+            addressResult.HouseNo = addressResult.HouseNo.LastIndexOf(baseAddress.Town, StringComparison.InvariantCultureIgnoreCase) == addressResult.HouseNo.Length - baseAddress.Town.Length ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.Town.Length) : addressResult.HouseNo;
+            addressResult.HouseNo = addressResult.HouseNo.Trim(' ').Trim(',');
+
+            // remove AddressLine2
+            addressResult.HouseNo = addressResult.HouseNo.LastIndexOf(baseAddress.AddressLine2, StringComparison.InvariantCultureIgnoreCase) == addressResult.HouseNo.Length - baseAddress.AddressLine2.Length ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.AddressLine2.Length) : addressResult.HouseNo;
+            addressResult.HouseNo = addressResult.HouseNo.Trim(' ').Trim(',');
+
+            // remove AddressLine1
+            addressResult.HouseNo = addressResult.HouseNo.LastIndexOf(baseAddress.AddressLine1, StringComparison.InvariantCultureIgnoreCase) == addressResult.HouseNo.Length - baseAddress.AddressLine1.Length ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.AddressLine1.Length) : addressResult.HouseNo;
+            addressResult.HouseNo = addressResult.HouseNo.Trim(' ').Trim(',').Trim(Convert.ToChar("\t"));
+
+            return addressResult;
         }
     }
 }
