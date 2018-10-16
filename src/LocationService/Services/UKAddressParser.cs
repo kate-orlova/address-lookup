1ec7e819e647a400a775e323683e010e63e4f81d
Kate Orlova <kate.orlova@deleteagency.com>
2018-10-17 00:02:11 +0100

    Added comments to the address parsing method

--- src/LocationService/Services/UKAddressParser.cs
+++ src/LocationService/Services/UKAddressParser.cs
@@ -6,6 +6,12 @@ namespace LocationService.Services
 {
     public class UKAddressParser : IAddressParser
     {
+        /// <summary>
+        /// Parses an address string to the Address structure
+        /// </summary>
+        /// <param name="addressString"></param>
+        /// <param name="baseAddress"></param>
+        /// <returns></returns>
         public Address Parse(string addressString, Address baseAddress)
         {
             var addressResult = new Address()
@@ -23,19 +29,37 @@ namespace LocationService.Services
             addressResult.HouseNo = addressResult.HouseNo.Substring(addressResult.PostCode.Length);
 
             // remove county
-            addressResult.HouseNo = addressResult.HouseNo.IndexOf(baseAddress.County, StringComparison.InvariantCultureIgnoreCase) == addressResult.HouseNo.Length - baseAddress.County.Length ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.County.Length) : addressResult.HouseNo;
+            addressResult.HouseNo =
+                addressResult.HouseNo.IndexOf(baseAddress.County, StringComparison.InvariantCultureIgnoreCase) ==
+                addressResult.HouseNo.Length - baseAddress.County.Length
+                    ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.County.Length)
+                    : addressResult.HouseNo;
             addressResult.HouseNo = addressResult.HouseNo.Trim(' ').Trim(',');
 
             // remove town
-            addressResult.HouseNo = addressResult.HouseNo.LastIndexOf(baseAddress.Town, StringComparison.InvariantCultureIgnoreCase) == addressResult.HouseNo.Length - baseAddress.Town.Length ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.Town.Length) : addressResult.HouseNo;
+            addressResult.HouseNo =
+                addressResult.HouseNo.LastIndexOf(baseAddress.Town, StringComparison.InvariantCultureIgnoreCase) ==
+                addressResult.HouseNo.Length - baseAddress.Town.Length
+                    ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.Town.Length)
+                    : addressResult.HouseNo;
             addressResult.HouseNo = addressResult.HouseNo.Trim(' ').Trim(',');
 
             // remove AddressLine2
-            addressResult.HouseNo = addressResult.HouseNo.LastIndexOf(baseAddress.AddressLine2, StringComparison.InvariantCultureIgnoreCase) == addressResult.HouseNo.Length - baseAddress.AddressLine2.Length ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.AddressLine2.Length) : addressResult.HouseNo;
+            addressResult.HouseNo =
+                addressResult.HouseNo.LastIndexOf(baseAddress.AddressLine2,
+                    StringComparison.InvariantCultureIgnoreCase) ==
+                addressResult.HouseNo.Length - baseAddress.AddressLine2.Length
+                    ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.AddressLine2.Length)
+                    : addressResult.HouseNo;
             addressResult.HouseNo = addressResult.HouseNo.Trim(' ').Trim(',');
 
             // remove AddressLine1
-            addressResult.HouseNo = addressResult.HouseNo.LastIndexOf(baseAddress.AddressLine1, StringComparison.InvariantCultureIgnoreCase) == addressResult.HouseNo.Length - baseAddress.AddressLine1.Length ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.AddressLine1.Length) : addressResult.HouseNo;
+            addressResult.HouseNo =
+                addressResult.HouseNo.LastIndexOf(baseAddress.AddressLine1,
+                    StringComparison.InvariantCultureIgnoreCase) ==
+                addressResult.HouseNo.Length - baseAddress.AddressLine1.Length
+                    ? addressResult.HouseNo.Substring(0, addressResult.HouseNo.Length - baseAddress.AddressLine1.Length)
+                    : addressResult.HouseNo;
             addressResult.HouseNo = addressResult.HouseNo.Trim(' ').Trim(',').Trim(Convert.ToChar("\t"));
 
             return addressResult;
