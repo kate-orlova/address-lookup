496bc56677d44112b4fa07dd1b57b8e4083caad5
Kate Orlova <kate.orlova@deleteagency.com>
2018-10-18 08:00:03 +0100

    added an address provider parser

---
+++ src/LocationService/Services/UKAddressProviderParser.cs
@@ -0,0 +1,30 @@
+﻿namespace LocationService.Services
+{
+    public class UKAddressProviderParser
+    {
+        private static UKAddressProviderParser _ukAddressProviderParser = new UKAddressProviderParser();
+        private static object _locker = new object();
+
+        private UKAddressProviderParser()
+        {
+        }
+
+        public static UKAddressProviderParser Instance
+        {
+            get
+            {
+                lock (_locker)
+                {
+                    if (_ukAddressProviderParser == null)
+                    {
+                        lock (_locker)
+                        {
+                            _ukAddressProviderParser = new UKAddressProviderParser();
+                        }
+                    }
+                }
+                return _ukAddressProviderParser;
+            }
+        }
+    }
+}
